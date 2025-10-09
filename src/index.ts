4a846b4e856f74a728a5c328f195a96448248ea2
Binbang <bintang.arya.mahendr@gmail.com>
2025-10-09 20:58:13 +0700

    Update chatbox avatar image and enhance chat functionality with DOM readiness checks

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ const app = new Elysia()
 .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  ` Sybau ts pmo 🥀🥀 is running at ${app.server?.hostname}:${app.server?.port}`
 );
